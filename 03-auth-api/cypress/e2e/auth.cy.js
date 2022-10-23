61df14f97c4deac77895ab70fabbbf09723a5cba
Yuyu <yuchi@xfers.com>
2022-10-23 23:05:12 +0800

    feat: 🎸 Add negative test

--- 03-auth-api/cypress/e2e/auth.cy.js
+++ 03-auth-api/cypress/e2e/auth.cy.js
@@ -10,3 +10,13 @@ it("return 200 when we hit /register", () => {
     }
   );
 });
+
+it("return 400 when we hit /register with nobody", () => {
+  cy.request({
+    method: "POST",
+    url: "http://localhost:3000/api/user/register",
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).to.eq(400);
+  });
+});
