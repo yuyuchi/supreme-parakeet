b47bd0198147e043a2a90e423b3a9422dc2380c2
Yuyu <yuchi@xfers.com>
2022-10-23 23:20:26 +0800

    feat: 🎸 Remove duplication

--- 03-auth-api/cypress/e2e/auth.cy.js
+++ 03-auth-api/cypress/e2e/auth.cy.js
@@ -1,25 +1,26 @@
-it("/register creates user", () => {
-  let body = {
-    name: "TestName",
-    email: "foo@bar.com",
-    password: "Test0987",
-  };
-  cy.request("POST", "http://localhost:3000/api/user/register", body).then(
-    (response) => {
+describe("/user/register", () => {
+  const registerEndpoint = "http://localhost:3000/api/user/register";
+  it("creates user with valid body", () => {
+    let body = {
+      name: "TestName",
+      email: "foo@bar.com",
+      password: "Test0987",
+    };
+    cy.request("POST", registerEndpoint, body).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.name).to.eq("TestName");
       expect(response.body.email).to.eq("foo@bar.com");
       expect(response.body.password).to.eq("Test0987");
-    }
-  );
-});
+    });
+  });
 
-it("return 400 when we hit /register with nobody", () => {
-  cy.request({
-    method: "POST",
-    url: "http://localhost:3000/api/user/register",
-    failOnStatusCode: false,
-  }).then((response) => {
-    expect(response.status).to.eq(400);
+  it("return 400 when we hit /register with nobody", () => {
+    cy.request({
+      method: "POST",
+      url: registerEndpoint,
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
   });
 });
