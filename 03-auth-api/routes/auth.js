8756910bf6cf528c172cd8bce9bf060b603b6310
Yuyu <yuchi@xfers.com>
2022-10-23 23:58:02 +0800

    feat: 🎸 Extract validation

--- 03-auth-api/routes/auth.js
+++ 03-auth-api/routes/auth.js
@@ -1,17 +1,11 @@
 const route = require("express").Router();
 const User = require("../model/Users");
-const Joi = require("@hapi/joi");
-
-const validationSchema = Joi.object({
-  name: Joi.string().min(2).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
+const { DataValidation } = require("../dataValidation");
 
 // app.use('/api/register')
 route.post("/register", async (req, res) => {
   // Validate that the body content matches our requirements
-  const { error } = validationSchema.validate(req.body);
+  const { error } = DataValidation.registerValidation(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
