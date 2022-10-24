c97e88aff6bf41a331329a8c1afb087649381039
Yuyu <yuchi@xfers.com>
2022-10-24 18:04:19 +0800

    feat: 🎸 Add posts endpoint

--- 03-auth-api/index.js
+++ 03-auth-api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const authRoute = require("./routes/auth");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+// Import routes
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 // setup to read an environmental variable from file
 dotenv.config();
@@ -16,4 +19,5 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 app.use(express.json());
 // Route middleware
 app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
 app.listen(3000, () => console.log("Server is running"));
---
+++ 03-auth-api/routes/posts.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const verify = require("./verifyToken");
+
+// http://localhost:3001/api/posts
+// By adding the verify, we made this route private that requires a token
+router.get("/", verify, async (req, res) => {
+  res.json({
+    posts: {
+      title: "my first post",
+      description: `data you shouldn't be able to access`,
+    },
+  });
+});
+
+module.exports = router;
